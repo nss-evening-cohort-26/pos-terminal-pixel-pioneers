1d40cba5d1e11d3fc2636b171215e65cebe28753
Fletcher Moore <fletcherjmoore14@gmail.com>
2024-03-13 19:45:46 -0500

    Commiting sample data

--- utils/startApp.js
+++ utils/startApp.js
@@ -1,7 +1,9 @@
 import domBuilder from '../components/shared/domBuilder';
+import logoutButton from '../components/buttons/loginButton';
 
 const startApp = () => {
   console.warn(startApp);
   domBuilder();
+  logoutButton();
 };
 export default startApp;
