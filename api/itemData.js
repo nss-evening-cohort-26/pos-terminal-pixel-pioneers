f786bb4283735944ef8289e0322e137a83fa557a
Taylor Seager <taylorseager718@gmail.com>
2024-03-13 21:05:51 -0500

    api calls for CRUD for items and orders added

--- api/itemData.js
+++ api/itemData.js
@@ -1,1 +1,80 @@
 // ALL ITEM PROMISES
+// ALL PROMISES FOR ORDERS
+import client from '../utils/client';
+
+const endpoint = client.databaseURL;
+
+// get all items
+const getItems = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/items.json/?orderBy="uid"&equalTo="${uid}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application.json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+// gets single item
+const getSingleItems = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/items/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// create new item
+const createNewItems = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/items.json/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// update single item
+const updateItem = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/items/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+// delete single item
+const deleteItem = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/items/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+export {
+  getItems,
+  createNewItems,
+  updateItem,
+  deleteItem,
+  getSingleItems
+};
--- api/orderData.js
+++ api/orderData.js
@@ -1,1 +1,79 @@
 // ALL PROMISES FOR ORDERS
+import client from '../utils/client';
+
+const endpoint = client.databaseURL;
+
+// get all orders
+const getOrders = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/orders.json/?orderBy="uid"&equalTo="${uid}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application.json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+// get single order
+const getSingleOrder = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/orders/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// create new order
+const createNewOrder = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/orders.json/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// update single order
+const updateOrder = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/orders/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+// delete single order
+const deleteOrder = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/orders/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+export {
+  getOrders,
+  createNewOrder,
+  updateOrder,
+  deleteOrder,
+  getSingleOrder
+};
