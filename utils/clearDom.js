d9ab70d66dfe0c2a654c34d2ea0050e52c94eba5
zach colburn <colburn.zach7@gmail.com>
2024-03-16 12:48:37 -0500

    querySelector #store and #add-button commented out.

--- utils/clearDom.js
+++ utils/clearDom.js
@@ -1,7 +1,7 @@
 const clearDom = () => {
   console.warn();
-  document.querySelector('#store').innerHTML = '';
-  document.querySelector('#add-button').innerHTML = '';
+  // document.querySelector('#store').innerHTML = '';
+  // document.querySelector('#add-button').innerHTML = '';
   document.querySelector('#form-container').innerHTML = '';
   document.querySelector('#view').innerHTML = '';
 };
