0e26a68a6d05f59ab0ceac7a8a143192592aad1c
zach colburn <colburn.zach7@gmail.com>
2024-03-15 20:00:40 -0500

    started working on the viewOrder function, changed some names around. and updated the domString to reflect what we want to render on each card. Still need to get the orders to render.

--- utils/startApp.js
+++ utils/startApp.js
@@ -1,9 +1,14 @@
 import domBuilder from '../components/shared/domBuilder';
 import logoutButton from '../components/buttons/loginButton';
+import viewOrder from '../pages/viewOrder';
+import { getOrders } from '../api/orderData';
 
-const startApp = () => {
-  console.warn(startApp);
-  domBuilder();
-  logoutButton();
+const startApp = (uid) => {
+  // console.warn(startApp);
+  domBuilder(uid);
+  viewOrder(uid);
+  logoutButton(uid);
+
+  getOrders(uid).then((orders) => viewOrder(orders));
 };
 export default startApp;
