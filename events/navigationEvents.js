9ae25476554ae9977c38a7dd007ab07f8012bba6
Taylor Seager <taylorseager718@gmail.com>
2024-03-16 13:22:42 -0500

    navBar event listeners working

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,4 +1,22 @@
-const navigationEvents = () => {
-  console.warn();
+import { createNewOrder, getOrders } from '../api/orderData';
+
+const navigationEvents = (uid) => {
+  document.querySelector('#navigation').addEventListener('click', (e) => {
+    if (e.target.id.includes('create-order')) {
+      console.warn('create order clicked');
+      createNewOrder(uid);
+    }
+
+    if (e.target.id.includes('view-orders')) {
+      console.warn('view orders clicked');
+      getOrders(uid);
+    }
+
+    // if (e.target.id.includes('logo')) {
+    //   console.warn('home screen');
+    //   viewHomeScreen(uid);
+    // }
+  });
 };
+
 export default navigationEvents;
--- utils/startApp.js
+++ utils/startApp.js
@@ -4,11 +4,13 @@ import { getOrders } from '../api/orderData';
 import viewOrder from '../pages/viewOrder';
 import navBar from '../components/shared/navBar';
 import clearDom from './clearDom';
+import navigationEvents from '../events/navigationEvents';
 
 const startApp = (uid) => {
   domBuilder(); // BUILD THE DOM
   navBar();
   logoutButton(); // ADD THE LOGOUT BUTTON COMPONENT
+  navigationEvents();
 
   clearDom();
   // TODO: Put all orders on the DOM on App load
