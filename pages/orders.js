0a8a5152ecc2ad91c4956737a62880f95ddb01fa
Fletcher Moore <fletcherjmoore14@gmail.com>
2024-03-16 10:11:28 -0500

    Commiting orders.js file for everyone to view their cards

--- pages/orders.js
+++ pages/orders.js
@@ -1,5 +1,36 @@
-const showAllOrders = () => {
-  console.warn();
+import renderToDom from '../utils/renderToDom';
+import clearDom from '../utils/clearDom';
+
+const emptyOrders = () => {
+  const domString = '<h1>No Authors</h1>';
+  renderToDom('#store', domString);
 };
 
-export default showAllOrders;
+const showAllOrders = (array) => {
+  clearDom();
+
+  const btnString = '<button class="btn btn-success btn-lg mb-4" id="add-author-btn">Add An Author</button>';
+
+  renderToDom('#add-button', btnString);
+
+  let domString = '';
+  array.forEach((item) => {
+    domString += `
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">${item.name}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">${item.customerPhone}</h6>
+        <h6 class="card-text"${item.customerEmail}></h6>   
+        <hr>
+        <p class="card-text">${item.orderType}</p>
+        <i class="btn btn-success" id="view-author-btn--${item.firebaseKey}"><span class="fas fa-eye"></span></i>
+        <i class="btn btn-info" id="update-author--${item.firebaseKey}"><span class="fas fa-edit"></span></i>
+        <i class="btn btn-danger" id="delete-author-btn--${item.firebaseKey}"><span class="fas fa-trash-alt"></span></i>
+      </div>
+    </div>
+    `;
+  });
+  renderToDom('#store', domString);
+};
+
+export default { showAllOrders, emptyOrders };
--- utils/startApp.js
+++ utils/startApp.js
@@ -1,9 +1,10 @@
 import domBuilder from '../components/shared/domBuilder';
-import logoutButton from '../components/buttons/loginButton';
+import logoutButton from '../components/buttons/logoutButton';
 
 const startApp = () => {
-  console.warn(startApp);
-  domBuilder();
-  logoutButton();
+  domBuilder(); // BUILD THE DOM
+  logoutButton(); // ADD THE LOGOUT BUTTON COMPONENT
+
+  // TODO: Put all orders on the DOM on App load
 };
 export default startApp;
