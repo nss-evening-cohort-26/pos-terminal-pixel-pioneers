8b06e484d4674bc2d79de81bc88bebaacb7a381a
Ross Morgan <ross.morgan933@gmail.com>
2024-03-17 19:28:10 -0500

    Added buttons on homescreen

--- components/buttons/homePageButtons.js
+++ components/buttons/homePageButtons.js
@@ -1,5 +1,26 @@
-// CREATE ORDER BUTTON
+import { getOrders } from '../../api/orderData';
+import { showAllOrders } from '../../pages/orders';
 
-// VIEW ORDER BUTTON
+const homePageButtons = (uid) => {
+  const domString = '<h1></h1><button id="view-btn" class="btn-home btn-danger">View Orders</button><button id="create-btn" class="btn-home btn-danger">Create an Order</button><button id="rev-btn" class="btn-home btn-danger">View Revenue</button>';
+  document.querySelector('#home-container').innerHTML = domString;
+  document.querySelector('#view-btn').addEventListener('click', () => {
+    getOrders(uid).then((orders) => showAllOrders(orders));
+    console.warn('Clicked');
+  });
+};
 
-// TOTAL REVENUE BUTTON;
+// // CREATE ORDER BUTTON
+// const createButton = () => {
+//   const domString = '<button id="create-btn" class="btn btn-danger">Create an Order</button>';
+//   document.querySelector('#home-container').innerHTML = domString;
+//   // document.querySelector('#google-auth').addEventListener('click', signIn);
+// };
+
+// // TOTAL REVENUE BUTTON;
+// const revButton = () => {
+//   const domString = '<button id="rev-button" class="btn btn-danger">View Revenue</button>';
+//   document.querySelector('#home-container').innerHTML = domString;
+//   // document.querySelector('#google-auth').addEventListener('click', signIn);
+// };
+export default homePageButtons;
--- utils/startApp.js
+++ utils/startApp.js
@@ -1,16 +1,19 @@
 import domBuilder from '../components/shared/domBuilder';
 import logoutButton from '../components/buttons/logoutButton';
-import { getOrders } from '../api/orderData';
+// import { getOrders } from '../api/orderData';
 import navBar from '../components/shared/navBar';
 import clearDom from './clearDom';
-import { showAllOrders } from '../pages/orders';
+// import { showAllOrders } from '../pages/orders';
+import homePageButtons from '../components/buttons/homePageButtons';
 
 const startApp = (uid) => {
   domBuilder(); // BUILD THE DOM
   navBar();
   logoutButton(); // ADD THE LOGOUT BUTTON COMPONENT
   clearDom();
+  homePageButtons(uid);
+
   // TODO: Put all orders on the DOM on App load
-  getOrders(uid).then((orders) => showAllOrders(orders));
+  // getOrders(uid).then((orders) => showAllOrders(orders));
 };
 export default startApp;
