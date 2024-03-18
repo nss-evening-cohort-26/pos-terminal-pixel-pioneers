398c60e263757e67aeadd361d7b2639c43ec92b6
Ross Morgan <ross.morgan933@gmail.com>
2024-03-18 18:13:43 -0500

    merge

--- utils/startApp.js
+++ utils/startApp.js
@@ -4,26 +4,19 @@ import logoutButton from '../components/buttons/logoutButton';
 import navBar from '../components/shared/navBar';
 import clearDom from './clearDom';
 import navigationEvents from '../events/navigationEvents';
-import { showAllOrders } from '../pages/orders';
-import { showAllItems } from '../pages/items';
-import { getItems } from '../api/itemData';
 import homePageButtons from '../components/buttons/homePageButtons';
-
-import { domEvents, buttonEvents } from '../events/domEvents';
+import domEvents from '../events/domEvents';
 
 const startApp = (uid) => {
   domBuilder(); // BUILD THE DOM
   navBar();
   logoutButton(uid); // ADD THE LOGOUT BUTTON COMPONENT
   navigationEvents();
-  showAllItems(uid);
   homePageButtons();
   domEvents(uid);
-  buttonEvents(uid);
   clearDom();
 
   // TODO: Put all orders on the DOM on App load
-  getOrders(uid).then((orders) => showAllOrders(orders));
-  getItems(uid).then((items) => showAllItems(items));
+  // getOrders(uid).then((orders) => showAllOrders(orders));
 };
 export default startApp;
