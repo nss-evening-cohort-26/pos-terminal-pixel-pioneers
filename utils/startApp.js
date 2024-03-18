21ab46c9dab8a721d7015ead612bb6cbd1ecf4df
Taylor Seager <taylorseager718@gmail.com>
2024-03-18 17:51:18 -0500

    merging

--- utils/startApp.js
+++ utils/startApp.js
@@ -4,35 +4,24 @@ import logoutButton from '../components/buttons/logoutButton';
 import navBar from '../components/shared/navBar';
 import clearDom from './clearDom';
 import navigationEvents from '../events/navigationEvents';
-<<<<<<< HEAD
-import { showAllOrders } from '../pages/orders';
+// import { showAllOrders } from '../pages/orders';
 import { showAllItems } from '../pages/items';
-import { getItems } from '../api/itemData';
-=======
+// import { getItems } from '../api/itemData';
 import homePageButtons from '../components/buttons/homePageButtons';
 import domEvents from '../events/domEvents';
->>>>>>> ada83791257fab5bdeb36f626c41adac0b76dfab
 
 const startApp = (uid) => {
   domBuilder(); // BUILD THE DOM
   navBar();
   logoutButton(uid); // ADD THE LOGOUT BUTTON COMPONENT
   navigationEvents();
-<<<<<<< HEAD
   showAllItems(uid);
 
-=======
   homePageButtons();
   domEvents(uid);
->>>>>>> ada83791257fab5bdeb36f626c41adac0b76dfab
   clearDom();
 
   // TODO: Put all orders on the DOM on App load
-<<<<<<< HEAD
-  getOrders(uid).then((orders) => showAllOrders(orders));
-  getItems(uid).then((items) => showAllItems(items));
-=======
   // getOrders(uid).then((orders) => showAllOrders(orders));
->>>>>>> ada83791257fab5bdeb36f626c41adac0b76dfab
 };
 export default startApp;
