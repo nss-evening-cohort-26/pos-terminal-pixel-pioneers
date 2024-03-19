c0cf92ca82a9cd2141c607d17a34a3df23d929a1
zach colburn <colburn.zach7@gmail.com>
2024-03-18 19:22:02 -0500

    merge issues

--- utils/startApp.js
+++ utils/startApp.js
@@ -1,29 +1,30 @@
 import domBuilder from '../components/shared/domBuilder';
 import logoutButton from '../components/buttons/logoutButton';
-import { getOrders } from '../api/orderData';
+// import { getOrders } from '../api/orderData';
 import navBar from '../components/shared/navBar';
 import clearDom from './clearDom';
 import navigationEvents from '../events/navigationEvents';
-import { showAllOrders } from '../pages/orders';
-import { showAllItems } from '../pages/items';
-import { getItems } from '../api/itemData';
+// import { showAllOrders } from '../pages/orders';
+// import { showAllItems } from '../pages/items';
+// import { getItems } from '../api/itemData';
 import homePageButtons from '../components/buttons/homePageButtons';
-
 import { domEvents, buttonEvents } from '../events/domEvents';
+
+// import { domEvents, buttonEvents } from '../events/domEvents';
 
 const startApp = (uid) => {
   domBuilder(); // BUILD THE DOM
   navBar();
   logoutButton(uid); // ADD THE LOGOUT BUTTON COMPONENT
   navigationEvents();
-  showAllItems(uid);
+  // showAllItems(uid);
   homePageButtons();
   domEvents(uid);
   buttonEvents(uid);
   clearDom();
 
   // TODO: Put all orders on the DOM on App load
-  getOrders(uid).then((orders) => showAllOrders(orders));
-  getItems(uid).then((items) => showAllItems(items));
+// getOrders(uid).then((orders) => showAllOrders(orders));
+// getItems(uid).then((items) => showAllItems(items));
 };
 export default startApp;
